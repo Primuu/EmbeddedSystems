5d1de10517f340aa53b1b51970955a3070d8e711
Adam <trentowski.adam@gmail.com>
2023-04-18 18:29:51 +0200

    Update main.c

--- Task 1/main.c
+++ Task 1/main.c
@@ -9,11 +9,21 @@
 #define ONE_TENTH_VOLT 31
 #define ONE_HUNDREDTH_VOLT 3
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: File Scope Variables and Functions
-// *****************************************************************************
-// *****************************************************************************
+
+
+
+
+#include <p24fj128ga010.h>
+
+//Konfiguracja dla Explorer 16 z progr. icd2
+_CONFIG1(JTAGEN_OFF & GCP_OFF & GWRP_OFF & BKBUG_OFF & COE_OFF & FWDTEN_OFF)
+_CONFIG2(FCKSM_CSDCMD & OSCIOFNC_ON & POSCMOD_HS & FNOSC_PRI)
+
+#define SCALE 308L
+
+
+
+
 
 
 void Update_LCD ( void ) ;
@@ -140,6 +150,100 @@ void fun2(int d) {
     }
 }
 
+
+// OTHER VERSION WORKING WITH BUTTONS SWITCH
+
+unsigned int fun1(unsigned int counter) {
+	return counter = counter + 1;
+}
+
+unsigned int fun2(unsigned int counter) {
+	return counter = counter - 1;
+}
+
+unsigned int fun34(unsigned int counter) {
+	return counter ^(counter>>1);
+}
+
+unsigned int fun56(unsigned int counter) {
+	int result = 0;
+	int ten = counter / 10;
+	int one = counter % 10;
+	ten = ten << 4;
+	result = ten | one;
+	return result;
+}
+
+unsigned int fun7(unsigned int counter) {
+	static int direction = 1;
+	if(direction == 1) {
+		counter = counter<<1;
+	}
+	if(direction == 0) {
+		counter = counter>>1;
+	}
+
+	if(counter == 224) {
+		direction = 0;
+	}
+	if(counter == 7){
+		direction = 1;
+	}
+	return counter;
+}
+
+unsigned int fun8() {
+	static int stop = 128;
+	static int queue = 1;
+	unsigned int counter = 0;
+	static int delay = 0;
+
+	if(counter == 255) {
+		counter = 1;
+		queue = 1;
+		delay = 0;
+		stop = 128;
+	}
+	if(queue > stop) {
+        delay = delay | stop;
+		queue = 1;
+        counter = delay | queue;
+		stop = stop >> 1;
+		if(stop == 0) {
+            stop = 128;
+            delay = 0;
+		}
+	}
+    counter = delay | queue;
+	queue = queue << 1;
+
+	return counter;
+}
+
+
+int k_bit(int n, int k)
+{
+     return (n>>k)&1; // Sprawdz przesuniety bit za pomoca AND
+}
+
+int funct9(int result)// 6 bitowy generator liczb pseudolosowych oparty o konfigurację 1110011
+{
+ int n, k1,k2,rand;
+ n=result;
+ k1=k_bit(n, 0); // k1=0bit liczby n
+ k2=k_bit(n, 1); // k2=2bit liczby n
+ k1=k1^k2; // wynik XOR(k1,k2)
+ k2=k_bit(n, 4); // k2=4bit liczby n
+ k1=k1^k2; // wynik XOR(k1,k2)
+ k2=k_bit(n, 5); // k2=5bit liczby n
+ k1=k1^k2; // wynik XOR(k1,k2)
+ n=n>>1; // n= n przesuniete o 1bit w prawo
+ rand=k1<<5; // rand= k1 przesuniete o 5bitow w lewo
+ n=rand|n; // n= operacja OR(rand, n)
+ result = n; 
+ return result; //zwraca zawartosc n
+}
+
 int main ( void )
 {
 
@@ -174,11 +278,124 @@ int main ( void )
     LED_Enable(LED_D9);
     LED_Enable(LED_D10);
 
+
+
+
+
+    unsigned long i;
+    unsigned char display=0; //zmienna odpowiedzialna za wyświetlanie
+    unsigned int przelacznik=0; //zmienna odpowiedzialna za przełączanie podprogramów
+    unsigned int acctual=0; //zmienna która zawiera aktualną liczbe wyswietlania
+    unsigned int snake=7; //zmienna odpowiedzialna za wężyk, równa 7 ponieważ 7 w binarnych to 111
+    unsigned int prng=1; // zmienna odpowiedzialna za generator liczb pseudolosowych
+
+
+
+
     /* Infinite Loop */
     while ( 1 )
     {
-        fun1(250);
-       fun2();
+
+    //    fun1(250);
+    //    fun2();
+
+
+
+	if (PORTDbits.RD13 == 0){//sprawdz czy przycisk zostal użyty
+		if(przelacznik == 9){//przy ostatnim podprogramie gdy kliknie sie przycisk RD13 uruchamia podprogram 0 
+			przelacznik=0;
+		}
+		else{
+	    	przelacznik=przelacznik+1;// po klinieciu przycisku RD13 uruchamia kolejny podprogram
+		}
+	}
+	else if(PORTDbits.RD6 == 0){//sprawdz czy przycisk zostal użyty
+	    if(przelacznik == -1){//przy pierwszym podprogramie gdy kliknie sie przycisk RD6 uruchamia podprogram 8
+			przelacznik=8;
+		}
+		else{
+			przelacznik=przelacznik-1;// po klinieciu przycisku RD6 uruchamia poprzedni podprogram
+		}
+	}
+
+	// Ustawienie podprogramów:
+	if (przelacznik==0){ // 1. 8 bitowy licznik binarny zliczający w górę (0...255)
+		display=funct1(display);// wczytanie funkcji do wyswietlenia
+		acctual = display;// zapisanie aktualnej wyswietlanej liczby
+	}
+	else if (przelacznik==1){ // 2. 8 bitowy licznik binarny zliczający w dół (255...0)
+		display=funct2(display);// wczytanie funkcji do wyswietlenia
+		acctual = display;// zapisanie aktualnej wyswietlanej liczby
+	}
+	else if (przelacznik==2){ // 3. 8 bitowy licznik w kodzie Graya zliczający w górę (repr. 0...255)
+		display=funct34(acctual++);// wczytanie funkcji do wyswietlenia z ostatnia wyswietlana wartoscia ktora jest zwiekszana o 1
+		acctual = display;// zapisanie aktualnej wyswietlanej liczby
+	}
+	else if (przelacznik==3){ // 4. 8 bitowy licznik w kodzie Graya zliczający w dół (repr. 255...0)
+		display=funct34(acctual--);// wczytanie funkcji do wyswietlenia z ostatnia wyswietlana wartoscia ktora jest zmniejszana o 1
+		acctual = display;// zapisanie aktualnej wyswietlanej liczby
+	}
+	else if (przelacznik==4){ // 5. 2x4 bitowy licznik w kodzie BCD zliczający w górę (0...99)
+		if(acctual >=100){ //restart acctual do 0 gdy jest >= 100
+				acctual = 0;
+		}
+		display=funct56(acctual++);// wczytanie funkcji do wyswietlenia z ostatnia wyswietlana wartoscia ktora jest zwiekszana o 1
+	}
+	else if (przelacznik==5){ // 6. 2x4 bitowy licznik w kodzie BCD zliczający w dół (99...0)
+		if(acctual <=0){//restart acctual do 99 gdy jest <= 0
+				acctual = 99;
+		}
+		display=funct56(acctual--);// wczytanie funkcji do wyswietlenia z ostatnia wyswietlana wartoscia ktora jest zmniejszana o 1
+	}
+	else if (przelacznik==6){ // 7. 3 bitowy wężyk poruszający się lewo-prawo
+		display=snake;// wczytanie zmiennej snake do wyswietlania
+		snake=funct7(snake++);// zapis do zmiennej snake funkcji z wartoscia snake zwiekszajacą sie o 1
+	}
+	else if (przelacznik==7){ // 8 .Kolejka
+		display=funct8(); //wyswietlenie funkcji
+	}
+	else if (przelacznik==8){ // 9. 6 bitowy generator liczb pseudolosowych oparty o konfigurację 1110011
+		display=prng; // display pobiera wartosc zmiennej prng
+    	display = funct9(prng);// wczytanie funkcji do wyswietlenia z wartoscia prng
+    	if(prng>=63)//reatrt zmiennej prng do 0 gdy jest wieksza równa 63
+    	{
+        prng=0;
+    	}
+    	prng=display;//wczytanie wyswietlanej wartosci do prng
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
         /* check if time to update LCD with Timer data */
         if ( appData.rtc_lcd_update )
